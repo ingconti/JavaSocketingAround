53daae2908e692080177271d5114be63d80a2d5c
Gian Enrico Conti <ingconti@ingconti.com>
2018-05-06 09:36:37 +0200

    first commit

---
+++ src/ClientMain.java
@@ -0,0 +1,6 @@
+public class ClientMain {
+
+    public static void main(String[] args) {
+        System.out.println("Client started!");
+    }
+}
---
+++ src/ServerMain.java
@@ -0,0 +1,6 @@
+public class ServerMain {
+
+    public static void main(String[] args) {
+        System.out.println("Server started!");
+    }
+}
